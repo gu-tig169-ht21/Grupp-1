83c8713c5f3a7c9aa1f5a66bb12242f5ac4bd9d7
Josk95 <jonas.skoog95@gmail.com>
2021-12-07 17:24:14 +0100

    Updated UI for Sign in screen

--- lib/screens/authentication/sign_in.dart
+++ lib/screens/authentication/sign_in.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quizapp/screens/authentication/register.dart';
 
 class SignIn extends StatefulWidget {
   const SignIn({Key? key}) : super(key: key);
@@ -14,68 +15,63 @@ class _SignInState extends State<SignIn> {
       appBar: AppBar(
         title: const Text("Logga in"),
         centerTitle: true,
+        elevation: 0,
       ),
       body: Container(
-        //Style later on
-
+        margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 50),
         child: Form(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.end,
             children: [
               TextFormField(
-                style: TextStyle(color: Colors.black),
                 decoration: const InputDecoration(
                     icon: Icon(Icons.email, color: Colors.amber),
                     labelText: "Email",
-                    hintText: "Skriv din emailadress här",
-                    hintStyle: TextStyle(color: Colors.red),
                     fillColor: Colors.black,
                     filled: true),
               ),
-              SizedBox(height: 20),
+              const SizedBox(height: 10),
               TextFormField(
                 decoration: const InputDecoration(
                   icon: Icon(Icons.lock, color: Colors.amber),
                   labelText: "Lösenord",
-                  hintText: "Skriv ditt lösenord här",
                   fillColor: Colors.black,
                   filled: true,
                 ),
               ),
-              loggaInButton(),
               Container(
-                  width: double.infinity,
-                  child: TextButton(
-                    child: Text('Registrera'),
-                    onPressed: () {},
-                  ))
+                width: double.infinity,
+                margin: const EdgeInsets.only(top: 20),
+                child: TextButton(
+                  onPressed: () {},
+                  child: const Text(
+                    'Logga in',
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                  style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all(Colors.orange)),
+                ),
+              ),
+              Container(
+                width: double.infinity,
+                margin: const EdgeInsets.only(top: 5),
+                child: TextButton(
+                  onPressed: () => Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const Register())),
+                  child: const Text(
+                    'Registrera',
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                  style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all(Colors.indigo[800])),
+                ),
+              ),
             ],
           ),
         ),
-      ),
-    );
-  }
-
-  Widget loggaInButton() {
-    return Container(
-      margin: EdgeInsets.all(10),
-      width: double.infinity,
-      child: ElevatedButton(
-        onPressed: () {},
-        child: Text('Logga in'),
-        style: ElevatedButton.styleFrom(),
-      ),
-    );
-  }
-
-  Widget registreraButton() {
-    return Container(
-      margin: EdgeInsets.all(10),
-      width: double.infinity,
-      child: ElevatedButton(
-        onPressed: () {},
-        child: Text('Registrera'),
-        style: ElevatedButton.styleFrom(),
       ),
     );
   }

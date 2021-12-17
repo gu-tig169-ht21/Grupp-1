09420e58baa8cc6a74bf8a75ccdf983ca27d39dd
eiihpos <guslofveis@student.gu.se>
2021-12-17 14:22:13 +0100

    Simple UI på olika Gamescore result

--- lib/screens/quiz/game_score.dart
+++ lib/screens/quiz/game_score.dart
@@ -5,7 +5,9 @@ import 'package:quizapp/models/quiz.dart';
 import 'package:quizapp/models/user.dart';
 import 'package:confetti/confetti.dart';
 import 'package:quizapp/models/user.dart';
+import 'package:quizapp/screens/home/highscore.dart';
 import 'package:quizapp/services/user_service.dart';
+import '';
 
 class GameScore extends StatefulWidget {
   const GameScore({Key? key}) : super(key: key);
@@ -37,10 +39,11 @@ class _GameScoreState extends State<GameScore> {
   }
 
   bool? newHighScore = null;
+  late int currentHigh;
 
   Future<bool?> saveScore(String id, int points) async {
     // int currentHighScore = await getCurrentHighScore(id);
-    int currentHigh = await getCurrentHighScore(id);
+    currentHigh = await getCurrentHighScore(id);
     print(currentHigh);
     //Save HighScore only if Score is bigger than current
     if (points > currentHigh) {
@@ -61,18 +64,72 @@ class _GameScoreState extends State<GameScore> {
     String id = stateUser.uid;
 
     return Scaffold(
+        appBar: AppBar(
+          title:
+              Text('Gamescore', style: Theme.of(context).textTheme.headline1),
+        ),
         body: FutureBuilder(
-      future: saveScore(id, quizState.points),
-      builder: (context, newHigh) {
-        if (newHigh.data == true) {
-          return confetti();
-        } else if (newHighScore == null) {
-          return Text("Loading");
-        } else {
-          return Text("Ingen ny High");
-        }
-      },
-    ));
+          future: saveScore(id, quizState.points),
+          builder: (context, newHigh) {
+            if (newHigh.data == true) {
+              return newHighscoreView(quizState.points, currentHigh);
+            } else if (newHighScore == null) {
+              return Text("Loading");
+            } else {
+              return noNewHighscoreView(quizState.points, currentHigh);
+              //return newHighscoreView(quizState.points, currentHigh);
+            }
+          },
+        ));
+  }
+
+  Widget newHighscoreView(var newHighscore, var currentScore) {
+    return Padding(
+      padding: const EdgeInsets.all(15.0),
+      child: Column(
+        children: [
+          confetti(),
+          Text('NEW HIGHSCORE!', style: TextStyle(fontSize: 20)),
+          Container(height: 15),
+          Icon(
+            Icons.emoji_emotions,
+            size: 170,
+          ),
+          ListTile(
+            title: Text("Points received & your new highscore: "),
+            subtitle: Text("$newHighscore p"),
+          ),
+          ListTile(
+            title: Text("Previous score: "),
+            subtitle: Text("$currentScore p"),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget noNewHighscoreView(var points, var currentScore) {
+    return Padding(
+      padding: const EdgeInsets.all(15.0),
+      child: Column(
+        children: [
+          Text('No new highscore...', style: TextStyle(fontSize: 20)),
+          Container(height: 15),
+          Icon(
+            Icons.thumb_down,
+            size: 170,
+          ),
+          ListTile(
+            title: Text("Points received: "),
+            subtitle: Text("$points p"),
+          ),
+          ListTile(
+            title: Text("Current highscore: "),
+            subtitle: Text("$currentScore p"),
+          ),
+        ],
+      ),
+    );
   }
 
   Widget confetti() {

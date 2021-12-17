cab495d6839eb8dbc14e51f8e109ca6a74a30485
eiihpos <guslofveis@student.gu.se>
2021-12-17 10:10:38 +0100

    UI NewGame, GameScore, Home Co-authored-by: Joel Harrysson <joelharrysson@users.noreply.github.com>

--- lib/screens/quiz/game_score.dart
+++ lib/screens/quiz/game_score.dart
@@ -1,16 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:quizapp/models/quiz.dart';
+import 'package:quizapp/models/user.dart';
 
-class GameScore extends StatelessWidget {
+class GameScore extends StatefulWidget {
   const GameScore({Key? key}) : super(key: key);
 
+  @override
+  State<GameScore> createState() => _GameScoreState();
+}
+
+class _GameScoreState extends State<GameScore> {
   @override
   Widget build(BuildContext context) {
     var state = Provider.of<QuizModel>(context, listen: false);
 
     return Scaffold(
         appBar: AppBar(title: Text("Game Score")),
-        body: Text(state.points.toString()));
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            textFieldFormat(state),
+          ],
+        ));
+  }
+
+  Widget textFieldFormat(state) {
+    return Align(
+      child: Text(
+        'Your score: \n ${state.points}',
+        style: TextStyle(fontSize: 30),
+      ),
+    );
+  }
+
+  int getHighScoreFromDatabase(BuildContext context) {
+    return 1;
   }
 }
